683ff76178a187d16ca7854ac5aaa5a5267d2bff
Aidil F <aidilfbk@gmail.com>
2018-03-18 19:19:01 +0800

    Metatable dispatching with initial playlist structure

--- src/config/postinit/xupnpd_dynamic.lua
+++ src/config/postinit/xupnpd_dynamic.lua
@@ -1,4 +1,28 @@
 if reload_playlists ~= nil then
+	immutable_nil_table = setmetatable({}, {__newindex = function() end})
+	dynamic_elements = {
+		elements = function(t, k)
+			local retval = rawget(t, "_elements")
+			if type(retval) == "function" then
+				retval = retval()
+			end
+			if type(retval) ~= "table" then
+				return immutable_nil_table
+			end
+			return retval
+		end,
+		size = function(t, k)
+			return #t.elements
+		end,
+		plugin = "dynamic",
+		__index = function (t, k)
+			local retval = dynamic_elements[k]
+			if type(retval) == "function" then
+				retval = retval(t, k)
+			end
+			return retval
+		end
+	}
 	function dynamic_reload_playlists()
 		print("called")
 		native_reload_playlists()
